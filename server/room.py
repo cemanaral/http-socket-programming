a00d3f6987170c34fb07f50299d4cb90dd5a8be7
Cem Anaral <cemanaral@hotmail.com>
2022-12-10 14:07:33 +0300

    feat: call functions using endpoints

--- server/room.py
+++ server/room.py
@@ -2,4 +2,5 @@ from ast import Pass
 import server.server_base
 
 class RoomServer(server.server_base.ServerBase):
-    pass
+    def add(self, name):
+        return f"add method called with name={name}"
--- server/server_base.py
+++ server/server_base.py
@@ -1,12 +1,13 @@
 import socket
 
+
 class ServerBase:
     def __init__(self, host, port):
         self.db = {}
         self.host = host
         self.port = port
         self.sock = self.__create_socket()
-        
+
     def __create_socket(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -37,11 +38,15 @@ class ServerBase:
     def parse_arguments(self, endpoint):
         arguments = endpoint.split('?')[1:]
         if arguments:
-            return list(map(lambda a: a.split('=')[1], arguments[0].split('&')))
+            return list(map(lambda a: f"'{a.split('=')[1]}'", arguments[0].split('&')))
         return []
 
     def handle_request(self, data):
         endpoint = self.parse_endpoint(data)
         method = self.parse_method_name(endpoint)
         arguments = self.parse_arguments(endpoint)
+
+        func_call_string = f"self.{method}({','.join(arguments)})"
+        result = eval(func_call_string)
+        print(result)
         return f'HTTP/1.0 200 OK\n\n<h1>{self.__class__.__name__}</h1><h2>endpoint: {endpoint}</h2><h2>method: {method}</h2><h2>arguments: {str(arguments)}</h2>'
